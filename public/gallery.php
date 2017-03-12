bd4ff44b995890b2fd2d3fad1a79f7c42a6d7960
DivDennis <divia.deidre@outlook.com>
2017-03-12 16:08:16 -0500

    gallery structure completed

--- public/gallery.php
+++ public/gallery.php
@@ -13,8 +13,28 @@
 <?php include 'navbar.php' ?>
     
 <div class="row">
-  <div class="col-md-4">.col-md-4</div>
-  <div class="col-md-4">.col-md-4</div>
+
+<div class="col-md-4">
+  
+<a data-flickr-embed="true" data-header="true"  href="https://www.flickr.com/gp/148131166@N03/C56tU9" title="Caribbean Christian Center for the Deaf" target="_blank"><img src="https://c1.staticflickr.com/1/674/33093900712_23ff002819_n.jpg" width="320" height="180" alt="Caribbean Christian Center for the Deaf"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+<br>
+<br>
+<center><p>“I have found out that there ain’t no surer way to find out whether you like people or hate them than to travel with them.” - Mark Twain</p></center>
+<br>
+<center><a class="button" href="https://www.flickr.com/gp/148131166@N03/C56tU9" target="_blank">Click Here To See Full Album</a></center>
+ 
+</div>
+
+<div class="col-md-4">
+  
+<a data-flickr-embed="true" data-header="true"  href="https://www.flickr.com/photos/148131166@N03/albums/72157681081677405" title="Coming Soon"><img src="https://c1.staticflickr.com/3/2878/32429167904_3850737b69_n.jpg" width="320" height="240" alt="Coming Soon"></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+<br>
+<br>
+<center><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></center>
+<br>
+<center><a  class="button" href="https://www.flickr.com/photos/148131166@N03/albums/72157681081677405">Click Here To See Full Album</a></center>
+
+  </div>
   <div class="col-md-4">.col-md-4</div>
 </div>
 
