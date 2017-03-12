9a2595da9677e3be8b9149d12d25d8e4fa76b21a
DivDennis <divia.deidre@outlook.com>
2017-03-12 14:50:08 -0500

    structure

--- public/gallery.php
+++ public/gallery.php
@@ -1,3 +1,21 @@
 <?php session_start(); ?>
 <?php include 'header.php' ?>
 <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+
+<style>  
+
+
+
+</style>
+
+<body>
+
+<?php include 'navbar.php' ?>
+    
+<div class="row">
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4">.col-md-4</div>
+  <div class="col-md-4">.col-md-4</div>
+</div>
+
+</body>
