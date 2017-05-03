64de55a0646057702c9ec39adc5cee48fb6a2e7f
DivDennis <divia.deidre@outlook.com>
2017-05-03 09:59:55 -0500

    Awating lessons and about us

--- public/about-us.php
+++ public/about-us.php
@@ -1,20 +1,15 @@
 <?php include './nav.php' ?>
 <link rel="stylesheet" href="./css/style-us.css">
 
-<div class="top">
-<div class="vtdi-mand">
-<h2> VTDI Mandeville Code Club </h2>
-<br>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-<br>
-Rem voluptatibus.</p>
-</div>
+<body>
 
-<div class="program">
-<img src="./img/programmer.png">
-</div>
-</div>
+<div class="container">
 
-<div class="bottom">
+    <div class="text">
+    
+    </div> 
 
 </div>    
+
+
+</body>  
